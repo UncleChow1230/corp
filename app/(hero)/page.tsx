27948dd241fb2f2d31832e2828e5adf8c21d7a4b
Zinkschow <14910436+zinkschow@user.noreply.gitee.com>
2024-09-27 16:18:10 +0800

    init

--- app/(hero)/page.tsx
+++ app/(hero)/page.tsx
@@ -1,14 +1,16 @@
-import Image from 'next/image'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import Image from "next/image"
+import Header from "@/components/header"
+/* eslint-enable @typescript-eslint/no-unused-vars */
+
 import React from 'react'
 import homeSrc from '/public/images/home.jpg'
 import Hero from '../../components/hero'
-import Header from '../../components/header'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'Home',
 }
-
 
 export default function Page() {
   return (
